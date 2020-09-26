35a30e553f6bcb103272359e55b3c558566283b2
Ashwin <ashwinr513@gmail.com>
2020-09-27 03:18:49 +0530

    Initial Commit

---
+++ FlyAwayPortal/src/com/flyaway/dbutil/DBConnection.java
@@ -0,0 +1,23 @@
+package com.flyaway.dbutil;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+ private static Connection conn;
+ public static Connection getConnection() throws SQLException, ClassNotFoundException
+ {
+	//Class.forName("com.mysql.jdbc.Driver");//com.mysql.cj.jdbc.Driver
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	String url="jdbc:mysql://localhost:3306/flyaway?noAccessToProcedureBodies=true";
+	//String url="jdbc:mysql://0.tcp.ngrok.io:10500/simplilearn?noAccessToProcedureBodies=true";
+	//String url="jdbc:mysql://0.tcp.ngrok.io:19161/simplilearn?noAccessToProcedureBodies=true";
+	String username="root";
+	String password="admin";
+	
+			
+	return conn=DriverManager.getConnection(url, username, password);
+	 
+ }
+}
